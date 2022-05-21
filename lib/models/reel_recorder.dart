7e39d5a6441fad1c6580330c234964da5dbe61d8
KangRuiYu <48875810+KangRuiYu@users.noreply.github.com>
2022-05-21 09:58:36 -0700

    Updated recording screen UI

--- lib/models/reel_recorder.dart
+++ lib/models/reel_recorder.dart
@@ -1,16 +1,28 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:path_provider/path_provider.dart';
 
 class ReelRecorder extends ChangeNotifier {
   late CameraController controller;
+  bool get recording => _recording;
+  bool _recording = false;
+
   final CameraDescription _camera;
+  int _timeCount = 1;
+  int _imageCount = 1;
 
   ReelRecorder(this._camera) {
     _init();
   }
 
   void _init() async {
-    controller = CameraController(_camera, ResolutionPreset.max);
+    controller = CameraController(
+      _camera,
+      ResolutionPreset.max,
+      imageFormatGroup: ImageFormatGroup.jpeg,
+    );
 
     try {
       await controller.initialize();
@@ -38,15 +50,28 @@ class ReelRecorder extends ChangeNotifier {
     return controller.value.isInitialized;
   }
 
-  void startRecording() {
-    controller.startImageStream(
-      (CameraImage image) {
-        print(image.format);
+  void startRecording() async {
+    Directory? dir = await getExternalStorageDirectory();
+    if (dir == null) return;
+
+    await controller.startImageStream(
+      (CameraImage image) async {
+        if (++_timeCount % 10 == 0) {
+          File file = File('${dir.path}/image$_imageCount.jpeg');
+          await file.writeAsBytes(image.planes[0].bytes);
+          _imageCount++;
+          print('Saving ${file.path}');
+        }
       },
     );
+
+    _recording = true;
+    notifyListeners();
   }
 
-  void stopRecording() {
-    controller.stopImageStream();
+  void stopRecording() async {
+    await controller.stopImageStream();
+    _recording = false;
+    notifyListeners();
   }
 }
--- lib/screens/recording_screen.dart
+++ lib/screens/recording_screen.dart
@@ -1,5 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:car_reels/models/camera_infos.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:car_reels/models/reel_recorder.dart';
 import 'package:provider/provider.dart';
@@ -13,32 +14,94 @@ class RecordingScreen extends StatelessWidget {
       create: (context) => ReelRecorder(
         context.read<CameraInfos>().primaryCamera(),
       ),
-      child: Column(
-        children: [
-          const ReelPreview(),
-          ElevatedButton(
-            child: const Text('Record'),
-            onPressed: () {},
-          ),
-          ElevatedButton(
-            child: const Text('Cancel'),
-            onPressed: () => Navigator.pop(context),
-          ),
-        ],
+      child: Builder(builder: (context) {
+        if (context.select((ReelRecorder r) => r.initialized())) {
+          return SafeArea(
+            child: Scaffold(
+              body: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Container(
+                    height: double.infinity,
+                    width: double.infinity,
+                    child: CameraPreview(
+                      context.watch<ReelRecorder>().controller,
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: IconButton(
+                          icon: const Icon(Icons.cancel_outlined),
+                          iconSize: 30.0,
+                          color: Colors.white,
+                          splashColor: Colors.white,
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                      ),
+                      Spacer(),
+                      ControlPanel(),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        } else {
+          return Center();
+        }
+      }),
+    );
+  }
+}
+
+class ControlPanel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadiusDirectional.vertical(
+          top: Radius.circular(20),
+        ),
+      ),
+      width: double.infinity,
+      height: 140.0,
+      child: Center(
+        child: RecordButton(),
       ),
     );
   }
 }
 
-class ReelPreview extends StatelessWidget {
-  const ReelPreview();
-
+class RecordButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    if (context.watch<ReelRecorder>().initialized()) {
-      return CameraPreview(context.watch<ReelRecorder>().controller);
+    if (context.watch<ReelRecorder>().recording) {
+      return ElevatedButton(
+        child: const Icon(Icons.stop_outlined, size: 40),
+        style: ElevatedButton.styleFrom(
+          primary: Colors.red,
+          shape: const CircleBorder(),
+          padding: const EdgeInsets.all(15),
+        ),
+        onPressed: () {
+          context.read<ReelRecorder>().stopRecording();
+        },
+      );
     } else {
-      return const Expanded(child: Center());
+      return ElevatedButton(
+        child: const Icon(Icons.fiber_manual_record_outlined, size: 40),
+        style: ElevatedButton.styleFrom(
+          primary: Colors.red,
+          shape: const CircleBorder(),
+          padding: const EdgeInsets.all(15),
+        ),
+        onPressed: () {
+          context.read<ReelRecorder>().startRecording();
+        },
+      );
     }
   }
 }
